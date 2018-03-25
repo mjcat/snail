834a5b01c02d68f426fe63f1405a5d87c5e2c17d
mjcat <yueyang.guan@gmail.com>
2018-03-24 19:41:16 -0700

    adding vuex store

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import App from './App';
-import router from './router';
-
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+import App from './App';
+import router from './router';
+import store from './store';
 
 Vue.config.productionTip = false;
 
@@ -16,6 +17,7 @@ Vue.use(BootstrapVue);
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>',
 });
